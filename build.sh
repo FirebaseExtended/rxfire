eb04b8777b9800888ae711033bce9b21caa4cf9e
James Daniels <jamesdaniels@google.com>
2021-01-22 14:27:56 -0500

    Get it building

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ LATEST_TEST="^[^-]*$"
 
 if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
     OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\//}
-    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
-        NPM_TAG=latest
-    else
-        NPM_TAG=next
-    fi;
+#    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
+#        NPM_TAG=latest
+        NPM_TAG=exp
+#    else
+#        NPM_TAG=next
+#    fi;
 else
     OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-canary.$SHORT_SHA
     NPM_TAG=canary
