4727079e9c653b483b5ae7c67baaf9bd34af812f
James Daniels <jamesdaniels@google.com>
2021-05-11 13:56:48 -0400

    Storage operations now work as expected

--- storage/index.ts
+++ storage/index.ts
@@ -16,8 +16,8 @@
  */
 
 import firebase from 'firebase/app';
-import {Observable, from} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
+import { debounceTime, map, shareReplay } from 'rxjs/operators';
 
 type UploadTaskSnapshot = firebase.storage.UploadTaskSnapshot;
 type Reference = firebase.storage.Reference;
@@ -26,15 +26,38 @@ type StringFormat = firebase.storage.StringFormat;
 type UploadTask = firebase.storage.UploadTask;
 
 export function fromTask(
-    task: firebase.storage.UploadTask,
+  task: firebase.storage.UploadTask
 ): Observable<UploadTaskSnapshot> {
-  return new Observable<UploadTaskSnapshot>((subscriber) => {
+  return new Observable<UploadTaskSnapshot>(subscriber => {
     const progress = (snap: UploadTaskSnapshot): void => subscriber.next(snap);
     const error = (e: Error): void => subscriber.error(e);
     const complete = (): void => subscriber.complete();
-    task.on('state_changed', progress, error, complete);
-    return () => task.cancel();
-  });
+    // emit the current state of the task
+    progress(task.snapshot);
+    // emit progression of the task
+    const unsubscribeFromOnStateChanged = task.on('state_changed', progress);
+    // use the promise form of task, to get the last success snapshot
+    task.then(
+      snapshot => {
+        progress(snapshot);
+        complete();
+      },
+      e => {
+        progress(task.snapshot);
+        error(e);
+      }
+    );
+    // the unsubscribe method returns by storage isn't typed in the
+    // way rxjs expects, Function vs () => void, so wrap it
+    return function unsubscribe() {
+      unsubscribeFromOnStateChanged();
+    };
+  }).pipe(
+    // since we're emitting first the current snapshot and then progression
+    // it's possible that we could double fire synchronously; namely when in
+    // a terminal state (success, error, canceled). Debounce to address.
+    debounceTime(0)
+  );
 }
 
 export function getDownloadURL(ref: Reference): Observable<string> {
@@ -48,33 +71,39 @@ export function getMetadata(ref: Reference): Observable<any> {
 }
 
 export function put(
-    ref: Reference,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: any,
-    metadata?: UploadMetadata,
+  ref: Reference,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any,
+  metadata?: UploadMetadata
 ): Observable<UploadTaskSnapshot> {
-  return fromTask(ref.put(data, metadata));
+  return new Observable<UploadTaskSnapshot>(subscriber => {
+    const task = ref.put(data, metadata);
+    return fromTask(task).subscribe(subscriber).add(task.cancel);
+  }).pipe(shareReplay({ bufferSize: 1, refCount: true }));
 }
 
 export function putString(
-    ref: Reference,
-    data: string,
-    format?: StringFormat,
-    metadata?: UploadMetadata,
+  ref: Reference,
+  data: string,
+  format?: StringFormat,
+  metadata?: UploadMetadata
 ): Observable<UploadTaskSnapshot> {
-  return fromTask(ref.putString(data, format, metadata));
+  return new Observable<UploadTaskSnapshot>(subscriber => {
+    const task = ref.putString(data, format, metadata);
+    return fromTask(task).subscribe(subscriber).add(task.cancel);
+  }).pipe(shareReplay({ bufferSize: 1, refCount: true }));
 }
 
 export function percentage(
-    task: UploadTask,
+  task: UploadTask
 ): Observable<{
   progress: number;
   snapshot: UploadTaskSnapshot;
 }> {
   return fromTask(task).pipe(
-      map((s) => ({
-        progress: (s.bytesTransferred / s.totalBytes) * 100,
-        snapshot: s,
-      })),
+    map(s => ({
+      progress: (s.bytesTransferred / s.totalBytes) * 100,
+      snapshot: s
+    }))
   );
 }
