a4310fb2f6c7cbf0bcc905d4795389ab814980c5
James Daniels <jamesdaniels@google.com>
2021-08-19 14:29:31 -0400

    && the build.sh to capture errors

--- build.sh
+++ build.sh
@@ -4,19 +4,17 @@ LATEST_TEST="^[^-]*$"
 
 if [[ $GITHUB_REF =~ $TAG_TEST ]]; then
     OVERRIDE_VERSION=${GITHUB_REF/refs\/tags\//}
-#    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
-#        NPM_TAG=latest
-        NPM_TAG=exp
-#    else
-#        NPM_TAG=next
-#    fi;
+    if [[ $OVERRIDE_VERSION =~ $LATEST_TEST ]]; then
+        NPM_TAG=latest
+    else
+        NPM_TAG=next
+    fi;
 else
     OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-canary.$SHORT_SHA
     NPM_TAG=canary
 fi;
 
 npm --no-git-tag-version --allow-same-version -f version $OVERRIDE_VERSION
-yarn build
-
-echo "npm publish . --tag $NPM_TAG" > ./dist/publish.sh
-chmod +x ./dist/publish.sh
+yarn build &&
+    echo "npm publish . --tag $NPM_TAG" > ./dist/publish.sh &&
+    chmod +x ./dist/publish.sh
