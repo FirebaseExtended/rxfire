b316434f4b49ee70c30299061ee5924a6f604c32
KingDarBoja <stevenbojato04@gmail.com>
2025-10-25 09:44:35 -0500

    fix the eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,37 +2,41 @@
 
 /** Check out https://typescript-eslint.io/getting-started/ */
 
-import eslint from '@eslint/js';
+import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
   ...defineConfig([
-    eslint.configs.recommended,
+    {
+      name: 'ESLint JS - Recommended',
+      ...js.configs.recommended
+    },
     tseslint.configs.recommended,
-    [
-      {
-        languageOptions: {
-          globals: {
-            ...globals.browser,
-            ...globals.node,
-          },
-          parser: tseslint.parser,
-          parserOptions: {
-            projectService: true,
-            tsconfigRootDir: import.meta.dirname,
-            ecmaVersion: 12,
-            sourceType: 'module',
-          },
+    {
+      name: 'typescript-eslint - config parser',
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.node,
+        },
+        parser: tseslint.parser,
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+          ecmaVersion: 12,
+          sourceType: 'module',
         },
       },
-    ],
+    },
   ]),
   {
-    ignores: ['**/dist', 'eslint.config.mjs'],
+    name: 'Ignore folder and files',
+    ignores: ['**/dist', '**/eslint.config.mjs'],
   },
   {
+    name: 'typescript-eslint plugin - Custom rules',
     rules: {
       '@typescript-eslint/no-deprecated': 'error',
     },
