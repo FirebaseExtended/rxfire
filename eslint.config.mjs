0270255b592eba92fd6ebcd3df42f06431a6ffa5
KingDarBoja <30560560+KingDarBoja@users.noreply.github.com>
2026-09-25 16:33:37 -0500

    🔧 chore(eslint): require and valid jsdoc plugin.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import globals from 'globals';
+import jsdoc from 'eslint-plugin-jsdoc';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
@@ -18,6 +19,9 @@ export default defineConfig([
   {
     name: 'Project - TypeScript',
     files: ['**/*.ts'],
+    plugins: {
+      jsdoc,
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -36,6 +40,15 @@ export default defineConfig([
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { caughtErrors: 'none' }],
       '@typescript-eslint/no-wrapper-object-types': 'off',
+        'jsdoc/check-param-names': 'warn',
+        'jsdoc/require-jsdoc': 'warn',
+        'jsdoc/require-param': 'warn',
+        'jsdoc/require-param-description': 'warn',
+        'jsdoc/require-param-type': 'warn',
+        'jsdoc/require-returns': ['warn', { forceRequireReturn: true }],
+        'jsdoc/require-returns-description': 'warn',
+        'jsdoc/require-returns-type': 'warn',
+        'jsdoc/valid-types': 'warn',
     },
   },
 ]);
